53c59e2a03bc6485a52510e46683a2b3613062e7
mahan <mahan@DESKTOP-HOMUGP3>
2025-12-23 16:21:40 +0330

    update

--- Pardis.Infrastructure/Migrations/20251223125107_AddSlidersAndStories.Designer.cs
+++ Pardis.Infrastructure/Migrations/20251223125107_AddSlidersAndStories.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pardis.Infrastructure;
 
@@ -11,9 +12,11 @@ using Pardis.Infrastructure;
 namespace Pardis.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251223125107_AddSlidersAndStories")]
+    partial class AddSlidersAndStories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
